9a4d954c647287cd9eed32774d5f16fd9467a6dd
Visti Andresen <talpa@talpa.dk>
2017-07-22 18:41:22 +0200

    Some more testing

--- g2coregui.py
+++ g2coregui.py
@@ -1,5 +1,6 @@
 import serial;
-
+import json;
+import time;
 
 class g2coreProtocol:
     MAX_RX_LENGTH=1024*100;
@@ -17,14 +18,38 @@ class g2coreProtocol:
         if len(self.buffer) + len(input) > self.MAX_RX_LENGTH:
             self.buffer = bytearray();
         self.buffer = self.buffer + input;
-        if input.find("\n")!=-1:
-            print self.buffer;
 
+    def getLine(self):
+        newlinePos = self.buffer.find("\n")
+        if newlinePos == -1:
+            return None;
+        else:
+            lineAsJson = None;
+            line = self.buffer[0:newlinePos].decode('utf-8');
+            try:
+                lineAsJson = json.loads(line);
+            except:
+                {}
+            if lineAsJson != None:
+                if 'f' in lineAsJson:
+                    footer = lineAsJson['f'];
+            self.buffer = self.buffer[newlinePos+1:];
+            return lineAsJson;
 
+    def sendLine(self, data):
+        self.serialPort.write((data+"\n").encode('utf-8'));
 
 
 protocol = g2coreProtocol();
 
+time.sleep(1);
+protocol.sendLine('{"sr":n}');
+protocol.sendLine('{"xfr":100}');
+protocol.sendLine('{"gc":"G1 X1 F1000"}');
+
+#protocol.sendLine('{"gc":"G1 X2 F2"}');
 while(True):
     protocol.animate();
-            
+    line = protocol.getLine();
+    if (line != None):
+        print line;
