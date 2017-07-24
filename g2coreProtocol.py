99b975841758d4091663e6a83b4bd292ebd6331e
Visti Andresen <talpa@talpa.dk>
2017-07-24 18:32:41 +0200

    Added rtscts=True for the serial port (probably won't do much for USB ttyACM ports)

--- g2coreProtocol.py
+++ g2coreProtocol.py
@@ -7,12 +7,12 @@ class g2coreProtocol:
     def __init__(self):
         self.MAX_RX_LENGTH=1024*100
         self.RX_CUNCK_SIZE = 1024
-        self.MAX_TX_BUFFERS = 4
+        self.MAX_TX_BUFFERS = 1
         
         self.txBuffersInUse = 0
        
         self.buffer = bytearray();
-        self.serialPort = serial.Serial(port='/dev/ttyACM0', baudrate=115000, timeout=0.01);
+        self.serialPort = serial.Serial(port='/dev/ttyACM0', baudrate=115000, timeout=0.01, rtscts=True);
 
     def animate(self):
         input = self.serialPort.read(self.RX_CUNCK_SIZE);
@@ -39,11 +39,13 @@ class g2coreProtocol:
                     if self.txBuffersInUse<0:
                         self.txBuffersInUse=0
             self.buffer = self.buffer[newlinePos+1:];
+            #print "on rx "+str(self.txBuffersInUse)
             return line;
 
     def sendLine(self, data):
         self.serialPort.write((data+"\n").encode('utf-8'));
         self.txBuffersInUse += 1
+        #print "on tx "+str(self.txBuffersInUse)
 
     def hasFreeTxBuffers(self):
         if self.txBuffersInUse < self.MAX_TX_BUFFERS:
